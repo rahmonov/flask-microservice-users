dc3cc0ba222440ed514d38233575de7b497c2854
Jahongir Rahmonov  =) <jrahmonov2@gmail.com>
2017-11-24 15:22:20 +0500

    Add postgres service

--- project/config.py
+++ project/config.py
@@ -1,20 +1,25 @@
+import os
 
 
 class BaseConfig:
     """ Base Configuration """
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """ Development configuration """
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')

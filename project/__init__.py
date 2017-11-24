f3840bdc5f9535ff87ff2995457c1d76f3c945c2
Jahongir Rahmonov  =) <jrahmonov2@gmail.com>
2017-11-24 14:48:49 +0500

    Add dockerfile and docker-compose.yaml

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.config.from_object('project.config.DevelopmentConfig')
+
+
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
 @app.route('/ping', methods=['GET'])

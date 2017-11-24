1e5ecb8644255a9ebf9670aacfea0874b92452dc
Jahongir Rahmonov  =) <jrahmonov2@gmail.com>
2017-11-24 16:03:18 +0500

    Cover with tests

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 import unittest
 from flask_script import Manager
 from project import create_app, db
+from project.api.models import User
 
 app = create_app()
 manager = Manager(app)
@@ -22,6 +23,14 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+
+@manager.command
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email="michael@realpython.com"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.commit()
 
 
 if __name__ == '__main__':

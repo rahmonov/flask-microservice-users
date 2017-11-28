4529d3e059fef4866b5f78d6d1ecabb7299f7123
Jahongir Rahmonov <jrahmonov2@gmail.com>
2017-11-28 15:45:53 +0500

    Add auth app and related views

--- manage.py
+++ manage.py
@@ -55,8 +55,16 @@ def test():
 @manager.command
 def seed_db():
     """Seeds the database."""
-    db.session.add(User(username='michael', email="michael@realpython.com"))
-    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.add(User(
+        username='michael',
+        email='michael@realpython.com',
+        password='test'
+    ))
+    db.session.add(User(
+        username='michaelherman',
+        email='michael@mherman.org',
+        password='test'
+    ))
     db.session.commit()
 
 manager.add_command('db', MigrateCommand)
